c80c7e667b45f743d216f50c0816d596d88bede3
tmclagen <tmclagen@users.noreply.github.com>
2020-06-07 21:27:55 +0300

    User validation

--- src/views/User.vue
+++ src/views/User.vue
@@ -7,37 +7,35 @@
         <label class="form_input" :class="{'is-error': username_error}">
           <span class="form_input_title">Username</span>
           <input type="text" placeholder="i.e. rick_sanchez" v-model="username">
-          <span class="form_input_error" v-show="username_error == 'pattern'">Letters, digits and @.+-_ only.</span>
-          <span class="form_input_error" v-show="username_error == 'limit_min'">Requires at least {{ username_validation.min }} character.</span>
-          <span class="form_input_error" v-show="username_error == 'limit_max'">The limit of {{ username_validation.max }} characters has been exceeded.</span>
+          <span class="form_input_error" v-show="username_error">{{ username_error }}</span>
         </label>
       </div>
       <div class="form_block">
-        <label class="form_input">
+        <label class="form_input" :class="{'is-error': first_name_error}">
           <span class="form_input_title">First name</span>
           <input type="text" placeholder="Rick" v-model="first_name">
+          <span class="form_input_error" v-show="first_name_error">{{ first_name_error }}</span>
         </label>
       </div>
       <div class="form_block">
-        <label class="form_input">
+        <label class="form_input" :class="{'is-error': last_name_error}">
           <span class="form_input_title">Last name</span>
           <input type="text" placeholder="Sanchez" v-model="last_name">
+          <span class="form_input_error" v-show="last_name_error">{{ last_name_error }}</span>
         </label>
       </div>
       <div class="form_block">
         <label class="form_input" :class="{'is-error': password_error}">
           <span class="form_input_title">Password</span>
           <input type="password" placeholder="Secret key" v-model="new_password" name="new_password" autocomplete="new-password">
-          <span class="form_input_error" v-show="password_error == 'pattern'">At least 1 uppercase letter, 1 lowercase letter and 1 digit.</span>
-          <span class="form_input_error" v-show="password_error == 'limit_min'">Requires at least {{ password_validation.min }} characters.</span>
-          <span class="form_input_error" v-show="password_error == 'limit_max'">The limit of {{ password_validation.max }} characters has been exceeded.</span>
+          <span class="form_input_error" v-show="password_error">{{ password_error }}</span>
         </label>
       </div>
       <div class="form_block">
         <label class="form_input" :class="{'is-error': password_compare_error}">
           <span class="form_input_title">Repeat password</span>
           <input type="password" placeholder="Repeat secret key" v-model="repeat_password">
-          <span class="form_input_error" v-show="password_compare_error">Passwords don't match.</span>
+          <span class="form_input_error" v-show="password_compare_error">{{ password_compare_error }}</span>
         </label>
       </div>
       <div class="form_block">
@@ -74,6 +72,14 @@ export default {
         max: 128
       },
       password_error: false,
+      first_name_validation: {
+        max: 30
+      },
+      first_name_error: false,
+      last_name_validation: {
+        max: 150
+      },
+      last_name_error: false,
       password_compare_error: false
     }
   },
@@ -102,17 +108,17 @@ export default {
       this.username_error = false;
       if(this.username.length < this.username_validation.min)
       {
-        this.username_error = 'limit_min';
+        this.username_error = `Requires at least ${this.username_validation.min} character.`;
         return;
       }
       if(this.username.length > this.username_validation.max)
       {
-        this.username_error = 'limit_max';
+        this.username_error = `The limit of ${this.username_validation.max} characters has been exceeded.`;
         return;
       }
       if(!this.username_validation.pattern.test(this.username))
       {
-        this.username_error = 'pattern';
+        this.username_error = 'Letters, digits and @.+-_ only.';
         return;
       }
     },
@@ -120,17 +126,17 @@ export default {
       this.password_error = false;
       if(this.new_password.length < this.password_validation.min)
       {
-        this.password_error = 'limit_min';
+        this.password_error = `Requires at least ${this.password_validation.min} characters.`;
         return;
       }
       if(this.new_password.length > this.password_validation.max)
       {
-        this.password_error = 'limit_max';
+        this.password_error = `The limit of ${this.password_validation.max} characters has been exceeded.`;
         return;
       }
       if(!this.password_validation.pattern.test(this.new_password))
       {
-        this.password_error = 'pattern';
+        this.password_error = 'At least 1 uppercase letter, 1 lowercase letter and 1 digit.';
         return;
       }
     },
@@ -138,7 +144,23 @@ export default {
       this.password_compare_error = false;
       if(this.new_password !== this.repeat_password)
       {
-        this.password_compare_error = true;
+        this.password_compare_error = "Passwords don't match";
+      }
+    },
+    firstNameCheck() {
+      this.first_name_error = false;
+      if(this.first_name.length > this.first_name_validation.max)
+      {
+        this.first_name_error = `The limit of ${this.first_name_validation.max} characters has been exceeded.`;
+        return;
+      }
+    },
+    lastNameCheck() {
+      this.last_name_error = false;
+      if(this.last_name.length > this.last_name_validation.max)
+      {
+        this.last_name_error = `The limit of ${this.last_name_validation.max} characters has been exceeded.`;
+        return;
       }
     },
     saveUser() {
@@ -150,10 +172,12 @@ export default {
         is_active: this.is_active
       }
       this.usernameCheck();
+      this.firstNameCheck();
+      this.lastNameCheck();
       this.passwordCheck();
       this.passwordsCompare();
 
-      if(this.username_error || this.password_error || this.password_compare_error)
+      if(this.username_error || this.password_error || this.password_compare_error || this.first_name_error || this.last_name_error)
       {
         return;
       }
